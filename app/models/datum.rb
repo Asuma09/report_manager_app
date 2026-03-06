935a713198e850148918e3cd0fb1ce3f7b66a4bb
Asuma09 <172264557+Asuma09@users.noreply.github.com>
2026-03-06 18:28:37 +0900

    Add association between Task and Datum

--- app/models/datum.rb
+++ app/models/datum.rb
@@ -1,3 +1,4 @@
 class Datum < ApplicationRecord
   belongs_to :task
+  has_one_attached :file
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,3 @@
 class Task < ApplicationRecord
+    has_many :data_list, class_name: "Datum", dependent: :destroy
 end
